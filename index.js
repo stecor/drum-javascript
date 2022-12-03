8f6b20e899f6b2a0fa0d1a54351b837ad2b76643
Stefano Corra <stefano.corra@hotmail.com>
2022-12-03 17:44:49 -0500

    sound commit

--- index.js
+++ index.js
@@ -2,40 +2,49 @@
 //   alert('I got clicked W!')
 // })
 
+document.addEventListener('keydown', function (event) {
+  let keyPressed = event.key
+  soundKey(keyPressed)
+})
+
 for (var i = 0; i < document.querySelectorAll('.drum').length; i++) {
   document.querySelectorAll('.drum')[i].addEventListener('click', function () {
     var buttonClick = this.innerHTML
-    switch (buttonClick) {
-      case 'w':
-        var audio = new Audio('sounds/tom-1.mp3')
-        audio.play()
-        break
-      case 'a':
-        var audio = new Audio('sounds/tom-2.mp3')
-        audio.play()
-        break
-      case 's':
-        var audio = new Audio('sounds/tom-3.mp3')
-        audio.play()
-        break
-      case 'd':
-        var audio = new Audio('sounds/tom-4.mp3')
-        audio.play()
-        break
-      case 'j':
-        var audio = new Audio('sounds/snare.mp3')
-        audio.play()
-        break
-      case 'k':
-        var audio = new Audio('sounds/crash.mp3')
-        audio.play()
-        break
-      case 'l':
-        var audio = new Audio('sounds/kick-bass.mp3')
-        audio.play()
-        break
-      default:
-        break
-    }
+    soundKey(buttonClick)
   })
+}
+
+function soundKey(letterKey) {
+  switch (letterKey) {
+    case 'w':
+      var tom1 = new Audio('sounds/tom-1.mp3')
+      tom1.play()
+      break
+    case 'a':
+      var tom2 = new Audio('sounds/tom-2.mp3')
+      tom2.play()
+      break
+    case 's':
+      var tom3 = new Audio('sounds/tom-3.mp3')
+      tom3.play()
+      break
+    case 'd':
+      var tom4 = new Audio('sounds/tom-4.mp3')
+      tom4.play()
+      break
+    case 'j':
+      var snare = new Audio('sounds/snare.mp3')
+      snare.play()
+      break
+    case 'k':
+      var crash = new Audio('sounds/crash.mp3')
+      crash.play()
+      break
+    case 'l':
+      var kick = new Audio('sounds/kick-bass.mp3')
+      kick.play()
+      break
+    default:
+      break
+  }
 }
